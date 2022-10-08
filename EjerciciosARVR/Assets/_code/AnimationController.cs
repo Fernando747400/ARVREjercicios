f59d82997bae627be7d22fc35954ba8df7017671
Fernando747 <fernandojcr5@gmail.com>
2022-10-08 02:39:30 -0500

    QOL improvements
    Fighter now resets and has cooldown

--- EjerciciosARVR/Assets/_code/AnimationController.cs
+++ EjerciciosARVR/Assets/_code/AnimationController.cs
@@ -9,13 +9,24 @@ public class AnimationController : MonoBehaviour
     [SerializeField] private Animator _animator;
     [SerializeField] private sagdollController _ragdollController;
     [SerializeField] private Rigidbody _hipsRigidBody;
-    [SerializeField] private int _hitsToEndure;
+    [SerializeField] private int _hitsToEndure = 5;
+    [SerializeField ] private float _hitCoolDown = 2f;
+    [SerializeField] private float _ragdollTime = 5f;
+    [SerializeField] private GameObject _modelHips;
 
+    private bool _isRagdolling = false;
     private int _totalHits = 0;
+    private float _inCoolDown = 0f;
+    private float _ragdollTimer = 0f;
+
+    private Vector3 _initialHipsPos;
+    private Vector3 _initialHipsRot;
 
     private void Awake()
     {
         AddEvent();
+        _initialHipsPos = _modelHips.transform.localPosition;
+        _initialHipsRot = _modelHips.transform.localEulerAngles;
     }
 
     private void Start()
@@ -24,10 +35,27 @@ public class AnimationController : MonoBehaviour
         _animator.SetTrigger("Idle");
     }
 
+    private void FixedUpdate()
+    {
+        _inCoolDown += Time.deltaTime;
+        if (_isRagdolling)
+        {
+            _ragdollTimer += Time.deltaTime;
+            if (_ragdollTimer >= _ragdollTime)
+            {
+                _ragdollTimer = 0f;
+                Revive();
+            }
+        }
+    }
+
     private void GotCollision(GameObject collision)
     {
         Debug.Log("Collided with " + collision.gameObject.name);
         Debug.Log("Total hits " + _totalHits);
+        if (_inCoolDown < _hitCoolDown) return;
+        if (_isRagdolling) return;
+        _inCoolDown = 0f;
         _totalHits++;
         if (_totalHits < _hitsToEndure)
         {
@@ -37,6 +65,7 @@ public class AnimationController : MonoBehaviour
         {
             Debug.Log("Die");
             Die();
+            _isRagdolling = true;
         }
     }
 
@@ -62,9 +91,21 @@ public class AnimationController : MonoBehaviour
     private void Die()
     {
         ResetAllTriggers();
-        RemoveEvents();
         _hipsRigidBody.constraints = RigidbodyConstraints.None;
         _ragdollController.Toggle();
+    }
+    
+    private void Revive()
+    {
+        _isRagdolling = false;
+        _totalHits = 0;
+        _ragdollController.Toggle();
+        _modelHips.transform.position = _initialHipsPos;
+        _modelHips.transform.rotation = Quaternion.Euler(_initialHipsRot);
+        ResetAllTriggers();
+        _animator.SetTrigger("Idle");
+        _hipsRigidBody.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ;
+        //_hipsRigidBody.constraints = RigidbodyConstraints.FreezeRotationZ;
     }
 
     private void AddEvent()
