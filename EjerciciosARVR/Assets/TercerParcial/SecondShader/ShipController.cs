d5ec916b153b7b7fcaa6794787f77eee4401c29d
Fernando747 <fernandojcr5@gmail.com>
2022-11-08 00:57:38 -0600

    WorkingShips

--- EjerciciosARVR/Assets/TercerParcial/SecondShader/ShipController.cs
+++ EjerciciosARVR/Assets/TercerParcial/SecondShader/ShipController.cs
@@ -7,6 +7,19 @@ public class ShipController : MonoBehaviour
 {
     [Header("ForcePositions")]
     [SerializeField] private Transform[] _sails;
+    [SerializeField] private float _windForce;
+
+    [SerializeField] private Transform _rudder;
+    [SerializeField] private float _rudderForce;
+
+    public enum Heading
+    {
+        Forward,
+        Left,
+        Right
+    }
+
+    public Heading HeadingState;
 
     private Rigidbody _rigidBody;
 
@@ -19,7 +32,25 @@ public class ShipController : MonoBehaviour
     {
         foreach(var item in _sails)
         {
-            _rigidBody.AddForceAtPosition(item.transform.forward * 0.1f, item.position, ForceMode.Force);
-        }       
+            _rigidBody.AddForceAtPosition(item.transform.forward * _windForce, item.position, ForceMode.Force);
+        }
+
+        MoveRudder();
+    }
+
+    public void MoveRudder()
+    {
+        switch (HeadingState)
+        {
+            case Heading.Forward:
+                //_rigidBody.AddForceAtPosition(_rudder.position * _rudderForce, _rudder.position, ForceMode.Force);
+                break;
+            case Heading.Left:
+                _rigidBody.AddForceAtPosition(_rudder.right * _rudderForce, _rudder.position, ForceMode.Force);
+                break;
+            case Heading.Right:
+                _rigidBody.AddForceAtPosition(-_rudder.right * _rudderForce, _rudder.position, ForceMode.Force);
+                break;
+        }
     }
 }
