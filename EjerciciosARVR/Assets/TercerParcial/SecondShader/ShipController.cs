008eaefd58bf579c88620bc22303cac9b60ad0d9
AmerikeAzul14 <nikot92089@upshopt.com>
2022-11-08 15:20:07 -0600

    Itween

--- EjerciciosARVR/Assets/TercerParcial/SecondShader/ShipController.cs
+++ EjerciciosARVR/Assets/TercerParcial/SecondShader/ShipController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using TMPro;
 using UnityEngine;
 
@@ -11,6 +12,8 @@ public class ShipController : MonoBehaviour
 
     [SerializeField] private Transform _rudder;
     [SerializeField] private float _rudderForce;
+
+    [SerializeField] private GameObject _rudderWheel;
 
     public enum Heading
     {
@@ -26,6 +29,10 @@ public class ShipController : MonoBehaviour
     private void Start()
     {
         _rigidBody = this.gameObject.GetComponent<Rigidbody>();
+    }
+    private void Update()
+    {
+        ReadInput();
     }
 
     private void FixedUpdate()
@@ -52,5 +59,32 @@ public class ShipController : MonoBehaviour
                 _rigidBody.AddForceAtPosition(-_rudder.right * _rudderForce, _rudder.position, ForceMode.Force);
                 break;
         }
+    }
+
+    private void ReadInput()
+    {
+        if (_rudderWheel == null) return;
+
+        if (Input.GetKeyDown(KeyCode.A)) 
+        {
+            HeadingState = Heading.Left;
+            RotateWheel(new Vector3(0,0,45));
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            HeadingState = Heading.Right;
+            RotateWheel(new Vector3(0, 0, -45));
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            HeadingState = Heading.Forward;
+            RotateWheel(Vector3.zero);
+        }
+    }
+
+    private void RotateWheel(Vector3 degrees)
+    {
+        iTween.RotateTo(_rudderWheel, iTween.Hash("rotation", degrees, "islocal", true, "time", 4f));
     }
 }
